f1704190239c9bf257fb19c0fc6347d20446a13c
dennisveldhuizen <dennisveldhuizen@outlook.com>
2024-01-01 11:16:38 +0100

    Instellingen in settings.py bijgewerkt met relevante wijzigingen

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,4 +132,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://192.168.1.12:3000',
+    # De onderstaande url is online.
+    'https://firedragon1977.github.io',
 ]
